d1f76ff7f5310ccff5167b3cbb6448d0089e765a
Fernando Cleber de Souza <fernando.cleber@prognosys.com.br>
2020-06-19 20:50:20 -0300

    Update download_url

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ustropo/uttlv',
-    download_url='',
+    download_url='https://github.com/ustropo/uttlv/archive/uttlv_0_3_0.tar.gz',
     author='Fernando C. de Souza',
     author_email='cleberdsouza@gmail.com',
     license='MIT',
