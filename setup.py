03a55a07515b45cfa91e5aec43d91eabaddac222
Fernando Cleber de Souza <fernando.cleber@prognosys.com.br>
2020-06-19 20:45:12 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-# Utility function to read the README file.
-# https://pythonhosted.org/an_example_pypi_project/setuptools.html#setting-up-setup-py
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='prtlv',
-    version='0.2.2',
+    name='uttlv',
+    version='0.3.0',
     description='Python library for TLV objects',
-    long_description=read('README.md'),
-    author='Prognosys - Sensing & Predicting',
-    author_email='development@prognosys.com.br',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ustropo/uttlv',
+    download_url='',
+    author='Fernando C. de Souza',
+    author_email='cleberdsouza@gmail.com',
     license='MIT',
-    packages=['prtlv'],
+    packages=['uttlv'],
     install_requires=[],
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -31,12 +31,14 @@ setup(
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT license',
+        "Operating System :: OS Independent",
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
 
